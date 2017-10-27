54494e36b33503ce119246ae0ef3ba48f5a8af65
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2017-10-27 11:20:24 +0200

    Add marks and test_capsule_content_sync

--- tests/foreman/endtoend/test_bats_foreman.py
+++ tests/foreman/endtoend/test_bats_foreman.py
@@ -5,14 +5,17 @@
 """
 
 import requests
+import pytest
 
 from robottelo.cli.host import Host
 from robottelo.cli.proxy import Proxy
+from robottelo.cli.capsule import Capsule
 from robottelo.config import settings
 from robottelo.ssh import command
 
 
-def test_all():
+@pytest.mark.foreman_pipeline
+def test_puppet_self_registration():
     """
     Verify a host is registered after a puppet apply
     """
@@ -31,3 +34,43 @@ def test_all():
     assert settings.server.hostname in [proxy['name'] for proxy in Proxy().list()]
 
     assert settings.server.hostname in [host['name'] for host in Host().list()]
+
+
+@pytest.mark.katello_pipeline
+def test_capsule_content_sync():
+    """
+    Verify a capsule can sync content
+    """
+    proxies = Proxy().list(search='Feature = "Pulp Node"')
+    assert len(proxies) >= 1
+    proxy = proxies[0]
+
+    organization = 'Test Organization'
+
+    Capsule().content_add_lifecycle_environment(id=proxy['id'], environment='Library',
+                                                organization=organization)
+
+    Capsule().content_synchronize(id=proxy['id'])
+
+    url1 = "http://{PROXY_HOSTNAME}/pulp/repos/{ORGANIZATION_LABEL}/Library/{CONTENT_VIEW_LABEL}/custom/{PRODUCT_LABEL}/{YUM_REPOSITORY_LABEL}/{FILENAME}"
+    url2 = "http://{PROXY_HOSTNAME}/pulp/repos/{ORGANIZATION_LABEL}/Library/{CONTENT_VIEW_LABEL}/custom/{PRODUCT_LABEL}/{YUM_REPOSITORY_LABEL}/Packages/{FILENAME[0]}/{FILENAME}"
+
+    response = requests.get(url1.format(
+        PROXY_HOSTNAME=proxy['name'],
+        ORGANIZATION_LABEL=organization.replace(' ', '_'),
+        CONTENT_VIEW_LABEL='Test_CV',
+        PRODUCT_LABEL='Test_Product',
+        FILENAME="walrus-0.71-1.noarch.rpm",
+    ))
+
+    if response.status_code == 404:
+        response = requests.get(url2.format(
+            PROXY_HOSTNAME=proxy['name'],
+            ORGANIZATION_LABEL=organization.replace(' ', '_'),
+            CONTENT_VIEW_LABEL='Test_CV',
+            PRODUCT_LABEL='Test_Product',
+            FILENAME="walrus-0.71-1.noarch.rpm",
+        ))
+
+    response.raise_for_status()
+    assert response.content
